ee53179aca129dcfcea16c1069b77bf080cf800b
gukjin jang <jgj455@gmail.com>
2020-03-15 23:45:19 +0900

    Add: 기본 UI 구성

--- CallCenterContacts/CallCenterContacts/ViewController.swift
+++ CallCenterContacts/CallCenterContacts/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    lazy var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .systemBackground
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
+        setConstraints()
+        
     }
-
-
+    
+    func setConstraints() {
+        self.view.addSubview(tableView)
+        
+        tableView.snp.makeConstraints{ (make) in
+            make.width.height.equalTo(self.view.safeAreaLayoutGuide)
+            make.center.equalTo(self.view.safeAreaLayoutGuide)
+        }
+    }
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
